ec9e512d056670c5b1b58d7a2d28ddacf4a24aac
Beracira <g.beracira@gmail.com>
2017-01-22 23:32:28 -0500

    td added

--- kernel/contextswitch.h
+++ kernel/contextswitch.h
@@ -5,34 +5,9 @@
 
 #define KERNEL_STACK_START    0x01300000
 #define USER_STACK_START 	    0x01200000
-#define USER_STACK_SIZE       0x00010000
+#define USER_STACK_SIZE       0x00015000
 
 #define MAX_TASKS 16
-
-struct task_descriptor {
-  int tid;
-  int state;
-  int priority;
-  int parent_tid;
-  int * sp;
-  int * lr;
-  int return_value;
-  int spsr;
-  int started;
-};
-
-
-// Order matters
-struct kernel_stack {
-	int * usr_sp;
-	int * usr_lr;
-	int usr_r0;
-  int usr_spsr;
-  int num_tasks;
-  int started;
-
-  int * current_td;
-};
 
 void syscall(int code);
 
