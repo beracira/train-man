1d026451dc109587477ac96c1bae5e400d09d2a7
Beracira <g.beracira@gmail.com>
2017-01-24 15:21:01 -0500

    added call number

--- kernel/user_syscall.h
+++ kernel/user_syscall.h
@@ -1,6 +1,12 @@
 #ifndef USER_SYSCALL_H
 #define USER_SYSCALL_H
 
+#define CREATE      0
+#define MYTID       1
+#define MYPARENTPID 2
+#define PASS        3
+#define EXIT        4
+
 int Create(priority_t priority, void (*code) ( ) );
 
 int MyTid(void);
