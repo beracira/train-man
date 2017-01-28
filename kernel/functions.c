9e2c128db5c8e14e66a2d28cb17bd4ae4047cd58
Casey Liu <x272liu@uwaterloo.ca>
2017-01-28 17:24:07 -0500

    init files for message passing

--- kernel/functions.c
+++ kernel/functions.c
@@ -6,17 +6,20 @@
 
 void firsttask(void) {
 
+  bwprintf( COM2, "First user task created\n\r");
+  
   int tid = 0;
 
+  bwprintf( COM2, "Creating task: tid # %d\n\r", tid);
   tid = Create(P_LOW, the_other_task);
-  bwprintf( COM2, "Created task: tid # %d\n\r", tid);
+  bwprintf( COM2, "Creating task: tid # %d\n\r", tid);
   tid = Create(P_LOW, the_other_task);
-  bwprintf( COM2, "Created task: tid # %d\n\r", tid);
+  bwprintf( COM2, "Creating task: tid # %d\n\r", tid);
   tid = Create(P_HIGH, the_other_task);
-  bwprintf( COM2, "Created task: tid # %d\n\r", tid);
+  bwprintf( COM2, "Creating task: tid # %d\n\r", tid);
   tid = Create(P_HIGH, the_other_task);
-  bwprintf( COM2, "Created task: tid # %d\n\r", tid);
-  bwprintf( COM2, "First user task: exiting.\n\r");
+
+  bwprintf( COM2, "Exiting first user task.\n\r");
   Exit();
 }
 
@@ -24,5 +27,7 @@ void the_other_task(void){
   bwprintf(COM2, "My tid is: %d. My parent's tid is: %d.\n\r", MyTid(), MyParentTid());
   Pass();
   bwprintf(COM2, "My tid is: %d. My parent's tid is: %d.\n\r", MyTid(), MyParentTid());
+
+  bwprintf(COM2, "Exiting task (tid %d)\n\r", MyTid());
   Exit();
 }
